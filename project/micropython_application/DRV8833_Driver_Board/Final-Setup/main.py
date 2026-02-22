4557a0e70b6b128376d3c2ef44c9b08d1f6c0625
Artem <vinivitinartem1@gmail.com>
2026-02-22 11:22:29 +0100

    Artem. Final skript for demo and presantation

--- project/micropython_application/DRV8833_Driver_Board/Final-Setup/main.py
+++ project/micropython_application/DRV8833_Driver_Board/Final-Setup/main.py
@@ -1,74 +1,37 @@
-import gc
-import time
-from machine import I2C, Pin, PWM
-from micropython_bmi270 import bmi270
-import machine
-from machine import PDM_PCM, freq, AUDIO_PDM_24_576_000_HZ
+import gc								#garbage collector to evade memory leaks
+import time   							#importing time to know time
+from machine import I2C, Pin, PWM		#importing communication
+from micropython_bmi270 import bmi270	#importing provided IMU Driver library
+import machine							#importing machine
 
-#Initializing I2C
+""" Initializing I2C """
 i2c = I2C(scl='P0_2', sda='P0_3') 
 
 
-
-
-#Initialising IMU
+""" Initialising IMU """
 def IMU_Config () :
+    print ("IMU Configuring........")
     global bmi; bmi = bmi270.BMI270(i2c)
     bmi.acceleration_range = bmi270.ACCEL_RANGE_2G
+    print ("IMU Configured Succesfully")
 
-#print(gc.mem_free() - a)
-
-#Mic Config code taken from Material Detection Exmaple
-def Mic_Config () :
-    freq(AUDIO_PDM_24_576_000_HZ)
-    SAMPLE_RATE_HZ = 16000  # Desired sample rate in Hz
-    AUDIO_BUFFER_SIZE = 512  # Size of the audio buffer
-    global AUDIO_BITS_PER_SAMPLE; AUDIO_BITS_PER_SAMPLE = 16  # Dynamic range in bits
-    MICROPHONE_GAIN = 12  # Microphone gain setting(best prediction observed at 12)
-    DIGITAL_BOOST_FACTOR = 50.0  # Digital boost factor for input signal
-    IMAI_DATA_OUT_SYMBOLS = ["unlabelled", "air", "plastic", "plastic_out", "wood", "wood_out"]
-
-    # PDM_PCM configuration
-    clk_pin = "P10_4"
-    data_pin = "P10_5"
-    rx_buf = array.array('h', [0] * AUDIO_BUFFER_SIZE)
-    
-    print("PDM initializing....")
-    global pdm_pcm; pdm_pcm = PDM_PCM(
-        0,
-        sck=clk_pin,
-        data=data_pin,
-        sample_rate=SAMPLE_RATE_HZ,
-        decimation_rate=64,
-        bits=PDM_PCM.BITS_16,
-        format=PDM_PCM.MONO_LEFT,
-        left_gain=MICROPHONE_GAIN,
-        right_gain=MICROPHONE_GAIN,
-    )
-    pdm_pcm.init()
-    print("PDM initialized successfully")
-
-# Function to normalize sample into range [-1, 1]
-def sample_normalize(sample):
-    return sample / float(1 << (AUDIO_BITS_PER_SAMPLE - 1))
-
-
-
-
-
-# --- Configuration ---
+""" Configuring PWM Pins for driver board """
 def PWM_Config ():
+    print("Configuring PWM...")
     PIN_DIR_1     = 'P9_0'  # Connected to OUT1 on driver
     PIN_DIR_2     = 'P9_1'  # Connected to OUT2 on driver
 
-    # Setup the Direction Pins as standard Digital Outputs
+    """ Setup the Direction Pins as standard Digital Outputs """
     PWM_DUTY = 0
     PWM_FREQ = 1000
-    global dir_pin1; dir_pin1 = PWM(Pin(PIN_DIR_1), duty_u16=PWM_DUTY, freq=PWM_FREQ)
-    global dir_pin2; dir_pin2 = PWM(Pin(PIN_DIR_2), duty_u16=PWM_DUTY, freq=PWM_FREQ)
+    global dir_pin1;
+    dir_pin1 = PWM(Pin(PIN_DIR_1), duty_u16=PWM_DUTY, freq=PWM_FREQ)
+    global dir_pin2;
+    dir_pin2 = PWM(Pin(PIN_DIR_2), duty_u16=PWM_DUTY, freq=PWM_FREQ)
+    print ("PWM Configured Succesfully")
 
 
-# Defining Basic Control Functions
+""" Defining Basic Control Functions """
 def Motor_Set_Speed (speed) :
     if (speed <= 0 and speed >= -100) :
         duty = int((-speed * 65535) / 100)
@@ -88,18 +51,19 @@ def Motor_Set_Speed (speed) :
     print("ERROR : Illegal speed given")
         
 def Motor_Stop():
+    """ Coast Braking """
     dir_pin1.duty_u16(0)
     dir_pin2.duty_u16(0)
     print("Motor Stopped")
 
 def Motor_Hard_Break():
-    """Hard Braking. stops instantly."""
-    dir_pin1.duty_u16(1)
-    dir_pin2.duty_u16(1)
+    """ Hard Braking. stops instantly. """
+    dir_pin1.duty_u16(65535)
+    dir_pin2.duty_u16(65535)
     print("Motor Hard Braking")
 
-import deepcraft_model
-import array
+import deepcraft_model  #Importing model
+import array            #Importing arrays
 
 def Intialize_Model ():
     print("Initializing Model....")
@@ -109,29 +73,41 @@ def Intialize_Model ():
     global input_dim; input_dim = model.get_model_input_dim()
     global output_dim; output_dim = model.get_model_output_dim()
     print (f"input_dim = {input_dim}; output_dim = {output_dim}")
-    global output_buffer; output_buffer = array.array('f', [0.0] * len(IMAI_DATA_OUT_SYMBOLS))
     print("Model Initialized Succesfuly")
     
+    """Placing  all Config function in one spot"""
+def Init_and_Config () :
+    print ("Configuring the Device...")
+    IMU_Config();
+    PWM_Config ()
+    Intialize_Model();
+    print("Device Configured Succesfully")
+    
+#1. prediction rate is 10 hz. windows is  [20, 6]
+#we need to give 20 sets of 6 values every 0.1 seconds
+# so 200 hz sampling is needed
 
-#1. prediction rate is 10 hz. with [20, 6] window, 200 hz sampling is needed
-#
-accx = 0; accy = 0; accz = 0;
-gyrox = 0; gyroy = 0; gyroz = 0;
 SAMPLING_FREQ = 200 
 INTERVAL_US = 1000000 // SAMPLING_FREQ  # 100000 us (1s)
 
-input_buffer = array.array('f', [0.0] * 6)
+
+""" Allocating arrays for input and output of the model"""
+input_buffer = array.array('f', [0.0] * input_dim)
+output_buffer = array.array('f', [0.0] * output_dim)
+
 next_sample_time = 0
+"""Functioj to fill Input window of model and retrieve status"""
 def Get_Status():
-    # 2. Local "Cache" of the sensor methods (Faster lookup)
+
     global next_sample_time
-    for i in range(20):
-        # 3. This creates a tuple (Temporary garbage, but small)
+    
+    """loop to give 20 bathes of 6 values"""
+    for i in range(0, 20):
+        """ writing down  when next sample should start"""
         next_sample_time = time.ticks_add(time.ticks_us(), INTERVAL_US)
         a = bmi.acceleration
         g = bmi.gyro
         
-        # 4. Fill the pre-allocated buffer (Zero new RAM used here!)
         input_buffer[0] = a[0]
         input_buffer[1] = a[1]
         input_buffer[2] = a[2]
@@ -139,37 +115,35 @@ def Get_Status():
         input_buffer[4] = g[1]
         input_buffer[5] = g[2]
         
-        # 5. Enqueue the SAME buffer
+
         model.enqueue(input_buffer) 
         
+        """ Wating what is left for next sample to begin"""
         time.sleep_us(time.ticks_diff(next_sample_time, time.ticks_us()))
 
-    # 6. Force clean the sensor tuples after the loop
+    """ 6. Force clean the memory to prevent memory leaks
+        garbage collector is not really fast (cleans up every 100 ms?)
+        so fast allocation of small things could cause memory leak"""
     gc.collect()
     output_status = model.dequeue(output_buffer)
     
-    #print("labels : ", output_buffer)
-    # Ultra-fast manual check for 4 elements
-    m = 0
-    if output_buffer[1] > output_buffer[m]: m = 1
-    if output_buffer[2] > output_buffer[m]: m = 2
-    if output_buffer[3] > output_buffer[m]: m = 3
-    # m is now the index of the maximum float
+    
+    # Fast manual check for most 
+    m = 0 											#Unlabled
+    if output_buffer[1] > output_buffer[m]: m = 1	#Imbalance
+    if output_buffer[2] > output_buffer[m]: m = 2	#Working
+    if output_buffer[3] > output_buffer[m]: m = 3	#Impact
+
     return m
 
-def Init_and_Config () :
-    print ("Configuring the Device...")
-    IMU_Config();
-    PWM_Config ()
-    machine.freq(machine.AUDIO_PDM_24_576_000_HZ)
-    Intialize_Model();
-    print("Device Configured Succesfully")
+
     
 def main():
     print("Starting Main Program...")
-    Motor_Set_Speed(100)
-    time.sleep(4)
+    Motor_Set_Speed(100) # Spinning motor with 100%
+    time.sleep(4)        # waiting for the motor to reach steady state
     count = 0
+    """starting loop"""
     for i in range (0, 10000) :
         #time.sleep_ms(1000)
         #print("Free RAM:", gc.mem_free())
@@ -181,7 +155,9 @@ def main():
             print("Motor HIT something")
             print("EMERGENCY STOP")
             break
-        elif count > 13 :
+        """reducing amount of frequent printing with simple counter
+           (to prevent buffer overflow)""" 
+        elif count > 13 : 
             count = 0
             if status == 0 : #unlabled
                 print("Motor is NOT spinnig")
@@ -192,8 +168,9 @@ def main():
             elif status == 2 :
                 print("Motor is working normally")
                 
-    print ("Main Program Ended. Press USER to SystemExit Program again or Press RESET to Reset")   
+    print ("Main Program Ended. Press USER to Restart Main Program or Press RESET to Reset")   
 
+"""Configruing interrupt that call Main Program"""
 user_button = Pin('P5_2', Pin.IN, Pin.PULL_UP)
 is_running = 0
 def USER_Button_handler(pin):
@@ -208,4 +185,5 @@ if __name__ == "__main__":
     print("Press USER to start the Main Program")
     print("Free RAM:", gc.mem_free())
     while 3 > 2 :
+        """busy waiting for main program start"""
         pass
